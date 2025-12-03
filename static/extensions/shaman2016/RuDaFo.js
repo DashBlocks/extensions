1f1b1ff04b7516bb6d3d6a8b385714857bc39c3a
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-03 22:48:06 +0300

    Update RuDaFo.js

--- static/extensions/shaman2016/RuDaFo.js
+++ static/extensions/shaman2016/RuDaFo.js
@@ -9,27 +9,6 @@
   if (!Scratch.extensions.unsandboxed) {
     throw new Error("This Extension must run unsandboxed");
   }
-RuDaFo = {
-  "ToJson": function(f, r) {
-    if (f === 'object') {
-      RuDaFo.data.i = r.split(';'); RuDaFo.data.i4 = {}; for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i.length; RuDaFo.data.i2++) { RuDaFo.data.i3 = RuDaFo.data.i[RuDaFo.data.i2]; RuDaFo.data.i5 = RuDaFo.data.i3.split(':'); RuDaFo.data.i4[RuDaFo.data.i5[0]] = RuDaFo.data.i5[1]; }; return RuDaFo.data.i4;
-    } else if (f === 'array') {
-      RuDaFo.data.i = r.split('&'); RuDaFo.data.i4 = []; for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i.length; RuDaFo.data.i2++) { RuDaFo.data.i3 = RuDaFo.data.i[RuDaFo.data.i2];RuDaFo.data.i4.push(RuDaFo.data.i3) }; return RuDaFo.data.i4;
-    } else {
-      console.error(`RuDaFo error: Not RuDaFo type name '${f}'`)
-    }
-  },
-  "data": {},
-  "FromJson": function(f, r) {
-    if (f === 'object') {
-      RuDaFo.data.i = r; RuDaFo.data.i4 = ""; RuDaFo.data.i3 = Object.keys(RuDaFo.data.i); for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i3.length; RuDaFo.data.i2++) { RuDaFo.data.i5 = RuDaFo.data.i[RuDaFo.data.i3[RuDaFo.data.i2]]; RuDaFo.data.i4 = `${RuDaFo.data.i4}${RuDaFo.data.i3[RuDaFo.data.i2]}:${RuDaFo.data.i5};` }; return RuDaFo.data.i4
-    } else if (f === 'array') {
-      RuDaFo.data.i = r; RuDaFo.data.i4 = ""; for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i.length; RuDaFo.data.i2++) { RuDaFo.data.i4 = `${RuDaFo.data.i4}${RuDaFo.data.i[RuDaFo.data.i2]}&` }; return RuDaFo.data.i4
-    } else {
-      console.error(`RuDaFo error: Not RuDaFo type name '${f}'`)
-    }
-  },
-}
 
     class RuDaFoExt {
       getInfo() {
@@ -104,6 +83,27 @@ RuDaFo = {
           ],
         };
       }
+RuDaFo = {
+  "ToJson": function(f, r) {
+    if (f === 'object') {
+      RuDaFo.data.i = r.split(';'); RuDaFo.data.i4 = {}; for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i.length; RuDaFo.data.i2++) { RuDaFo.data.i3 = RuDaFo.data.i[RuDaFo.data.i2]; RuDaFo.data.i5 = RuDaFo.data.i3.split(':'); RuDaFo.data.i4[RuDaFo.data.i5[0]] = RuDaFo.data.i5[1]; }; return RuDaFo.data.i4;
+    } else if (f === 'array') {
+      RuDaFo.data.i = r.split('&'); RuDaFo.data.i4 = []; for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i.length; RuDaFo.data.i2++) { RuDaFo.data.i3 = RuDaFo.data.i[RuDaFo.data.i2];RuDaFo.data.i4.push(RuDaFo.data.i3) }; return RuDaFo.data.i4;
+    } else {
+      console.error(`RuDaFo error: Not RuDaFo type name '${f}'`)
+    }
+  },
+  "data": {},
+  "FromJson": function(f, r) {
+    if (f === 'object') {
+      RuDaFo.data.i = r; RuDaFo.data.i4 = ""; RuDaFo.data.i3 = Object.keys(RuDaFo.data.i); for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i3.length; RuDaFo.data.i2++) { RuDaFo.data.i5 = RuDaFo.data.i[RuDaFo.data.i3[RuDaFo.data.i2]]; RuDaFo.data.i4 = `${RuDaFo.data.i4}${RuDaFo.data.i3[RuDaFo.data.i2]}:${RuDaFo.data.i5};` }; return RuDaFo.data.i4
+    } else if (f === 'array') {
+      RuDaFo.data.i = r; RuDaFo.data.i4 = ""; for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i.length; RuDaFo.data.i2++) { RuDaFo.data.i4 = `${RuDaFo.data.i4}${RuDaFo.data.i[RuDaFo.data.i2]}&` }; return RuDaFo.data.i4
+    } else {
+      console.error(`RuDaFo error: Not RuDaFo type name '${f}'`)
+    }
+  },
+}
 block1(args) {
   return RuDaFo.FromJson('object', args.json)
 }
