dec15d2e4f851608da43d1f9b9b18fbf3a03edb3
Damir <dambul2809@mail.ru>
2026-01-13 05:56:31 +0000

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -97,7 +97,7 @@ const extensions = [
         id: "arduinoUltimate",
         description: "Allows you to control Arduino directly from the browser. Supports digital/analog I/O, PWM, Servo, I2C, basic sensors and devices.",
         code: "scratch_craft_2/ArduinoULTIMATE.js",
-        banner: "scratch_craft_2/ArduinoULTIMATE.png",
+        // banner: "scratch_craft_2/ArduinoULTIMATE.png",
         creator: { name: "scratch_craft_2", link: "_scratch_" },
         notes: "REQUIRED: The board must be flashed with StandardFirmata and the browser must support Web Serial (Chrome/Edge).",
     },
