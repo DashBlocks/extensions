865683bab0fbb29e116bfb1b7a72ac0223196329
Damir <dambul2809@mail.ru>
2026-04-01 05:52:12 +0000

    Modals.js: Clean up code :/

--- static/extensions/scratch_craft_2/Modals.js
+++ static/extensions/scratch_craft_2/Modals.js
@@ -1,72 +1,78 @@
 // Name: Modals
-// ID: htmlalert
+// ID: scratchCraft2Alert
 // Description: This extension adds modal dialog boxes.
 // By: scratch_craft_2 <https://scratch.mit.edu/users/scratch_craft_2/>
 // License: MPL-2.0
 
 (function (Scratch) {
-    "use strict";
+  "use strict";
 
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error("This Extension must run unsandboxed");
+  const Cast = Scratch.Cast;
+
+  class Modals {
+    getInfo() {
+      return {
+        id: "scratchCraft2Alert",
+        name: Scratch.translate("Modals"),
+        color1: "#a3c0e1",
+        blocks: [
+          {
+            opcode: "alertBlock",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("alert [TEXT]"),
+            arguments: {
+              TEXT: {
+                defaultValue: Scratch.translate("Hello!"),
+                type: Scratch.ArgumentType.STRING,
+              },
+            },
+          },
+          {
+            opcode: "promptBlock",
+            blockType: Scratch.BlockType.REPORTER,
+            text: Scratch.translate("prompt [TEXT] with default value [DEFAULT]"),
+            arguments: {
+              TEXT: {
+                defaultValue: Scratch.translate("What's your name?"),
+                type: Scratch.ArgumentType.STRING,
+              },
+              DEFAULT: {
+                defaultValue: Scratch.translate("Dashy"),
+                type: Scratch.ArgumentType.STRING,
+              },
+            },
+          },
+          {
+            opcode: "confirmBlock",
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: Scratch.translate("confirm [TEXT]"),
+            arguments: {
+              TEXT: {
+                defaultValue: Scratch.translate("Did you see that movie?"),
+                type: Scratch.ArgumentType.STRING,
+              },
+            },
+          },
+        ],
+      };
+    }
+
+    alertBlock (args) {
+      const text = Cast.toString(args.TEXT);
+      alert(text);
+    }
+
+    promptBlock (args) {
+      const text = Cast.toString(args.TEXT);
+      const defaultValue = Cast.toString(args.DEFAULT);
+      return prompt(text, defaultValue);
+    }
+
+    confirmBlock (args) {
+      const text = Cast.toString(args.TEXT);
+      return confirm(text);
+    }
   }
 
-    class modals {
-      getInfo() {
-        return {
-          id: "htmlalert",
-          name: "modals",
-          color1: "#a3c0e1",
-          blocks: [
-            {
-              opcode: "alertb",
-              blockType: Scratch.BlockType.COMMAND,
-              text: "alert [textia]",
-              arguments: {
-                textia: {
-                  defaultValue: "hello!",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           },
-          {
-              opcode: "Promptb",
-              blockType: Scratch.BlockType.REPORTER,
-              text: "prompt [textip], default value: [classic]",
-              arguments: {
-                textip: {
-                  defaultValue: "what's your name?",
-                  type: Scratch.ArgumentType.STRING,
-                },
-                  classic: {
-                  defaultValue: " ",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           },
-           { 
-              opcode: "Confirmb",
-              blockType: Scratch.BlockType.BOOLEAN,
-              text: "confirm [textic]",
-              arguments: {
-                textic: {
-                  defaultValue: "Did you see that movie?",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           },
-          ],
-        };
-      }
-alertb(args) {
- alert([args.textia]);
-}
-Promptb(args) {
-  return prompt([args.textip], [args.classic]);
-}
-Confirmb(args) {
-  return confirm([args.textic]);
-}
-    }
-    Scratch.extensions.register(new modals());
-  })(Scratch);
+  Scratch.extensions.register(new Modals());
+})(Scratch);
